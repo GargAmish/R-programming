480e27470ef5e2e8b9014c8310b8ba5db822682c
GargAmish <162088526+GargAmish@users.noreply.github.com>
2024-03-23 12:31:25 +0530

    Add files via upload

---
+++ Palmer Penguins.R
@@ -0,0 +1,7 @@
+install.packages("palmerpenguins")
+library("palmerpenguins")
+install.packages("ggplot2")
+library("ggplot2")
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point()
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(color="purple")
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g)) + geom_point(aes(color=species,shape=species)) + facet_wrap(~species) + labs(title="Palmer Penguins: Body Mass Vs Flipper Length")
